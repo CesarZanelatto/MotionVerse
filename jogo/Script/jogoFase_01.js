9e841d3a464a5c8c8a46781416fe201957043a81
Kleber Amaro <kleberamarosilva@gmail.com>
2026-08-19 11:54:38 -0300

    Refresh fase 01 layout and scene assets
    Updates fase_01 to use a new background image across the registry, phase data, and HTML markup, and expands the platform tile set with new classroom/computer-area props. Adds the corresponding image assets under cenario/editor_uploads and updates eletrica art files. Also removes direct background-music autoplay setup from jogoFase_01.js.

--- jogo/Script/jogoFase_01.js
+++ jogo/Script/jogoFase_01.js
@@ -3,13 +3,3 @@ import Fase01 from "../Data/fase_01.js";
 
 const fase = Fase01;
 iniciarFase(fase);
-
-const audioFundo = new Audio("../som/fase1musicaFundo.mp3");
-audioFundo.loop = true;
-
-document.addEventListener("click", () => {
-  audioFundo.play().catch(() => {});
-}, { once: true });
-
-
-
